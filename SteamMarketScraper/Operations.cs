c435ffb0965650b1ef83c151ffa4b4aa67869bca
habinaattori <eero.peli04@gmail.com>
2018-11-19 16:31:32 +0200

    Image Downloader Added
    Image downloader that save images to the DownloadedFiles folder.

--- SteamMarketScraper/Operations.cs
+++ SteamMarketScraper/Operations.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace SteamMarketScraper
 {
@@ -14,35 +18,39 @@ namespace SteamMarketScraper
 
         public void ScraperStarter()
         {
-            OpenBrowser();
+            IWebDriver OGDriver = new ChromeDriver();
 
             //Go to CSGO's marketplace
-            driver.Navigate().GoToUrl("https://steamcommunity.com/market/search?appid=730#p1_price_asc");
+            OGDriver.Navigate().GoToUrl("https://steamcommunity.com/market/search?appid=730#p1_price_asc");
 
             //Wait two seconds so that the webpage has time to load
-            System.Threading.Thread.Sleep(4000);
+            System.Threading.Thread.Sleep(2000);
 
             //Search for the products' names
-            IList<IWebElement> productList = driver.FindElements(By.CssSelector(".market_listing_item_name"));
-            IList<IWebElement> imgLinkList = driver.FindElements(By.CssSelector(".market_listing_item_img"));
+            IList<IWebElement> productList = OGDriver.FindElements(By.CssSelector(".market_listing_item_name"));
+            IList<IWebElement> imgLinkList = OGDriver.FindElements(By.CssSelector(".market_listing_item_img"));
             string[] imgSrcList = new string[imgLinkList.Count];
 
             Console.WriteLine("productList: " + productList.Count);
             Console.WriteLine("imgLinkList: " + imgLinkList.Count);
             Console.WriteLine("imgSrcList: " + imgSrcList.Length);
 
-            for (int i = 0; i < imgSrcList.Length; i++)
+            int productNumber = 0;
+
+            for(int i = 0; i < imgSrcList.Length; i++)
             {
                 imgSrcList[i] = imgLinkList[i].GetAttribute("src");
-
                 OpenBrowser();
-
                 driver.Navigate().GoToUrl(imgSrcList[i]);
-
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(imgSrcList[i], "a" + productNumber + ".jpeg");
+                productNumber++;
                 CloseBrowser();
             }
 
-            Console.Read();    
+            OGDriver.Close();
+
+            Console.Read();
         }
 
 
@@ -62,3 +70,11 @@ namespace SteamMarketScraper
         }
     }
 }
+
+
+    //Make the code have comments
+
+    //Be sure to not get chrome to download anything
+
+
+
